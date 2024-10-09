4d538eeef003729ea0d752ee09087c7688292781
Ananta Nimnual <ananta_n@cmu.ac.th>
2024-10-10 03:17:14 +0700

    init begin lab11 from tew's lab10+auction

---
+++ src/main/java/se331/lab/rest/entity/Event.java
@@ -0,0 +1,35 @@
+package se331.lab.rest.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+import se331.lab.rest.entity.Organizer;
+
+import java.util.List;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Exclude
+    Long id;
+    String category;
+    String title;
+    String description;
+    String location;
+    String date;
+    String time;
+    Boolean petsAllowed;
+    @ManyToOne
+    Organizer organizer;
+    @ManyToMany(mappedBy = "eventHistory")
+    List<Participant> participants;
+}
